65eb783e74d14a458c27c4324be84d9d7ad0e3a2
Paul <paul.sugar.a@gmail.com>
2020-07-24 14:41:49 +0400

    Добавил 2 основных класса Room и Generator

---
+++ Assets/Scripts/Generator/Generator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GridGenerator
+{
+    public class Generator
+    {
+        private bool[,] _grid;
+        private Vector2Int _gridSize;
+        private Room[] _rooms;
+        public Generator(List<Room> rooms, Vector2Int gridSize)
+        {
+            rooms.CopyTo(_rooms);
+            _gridSize = new Vector2Int(gridSize.x, gridSize.y);
+            _grid = new bool[gridSize.x, gridSize.y];
+        }
+        public void Build()
+        {
+            
+        }
+    }
+}
+
---
+++ Assets/Scripts/Generator/Room.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GridGenerator
+{
+    public class Room
+    {
+        public Vector2Int Size;
+    }
+}
